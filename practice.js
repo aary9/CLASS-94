0ca5d84a0c5f5cd75605087974524b8e654b082d
aary9 <78476344+aary9@users.noreply.github.com>
2021-05-28 14:52:49 +0530

    Add files via upload

---
+++ practice.js
@@ -0,0 +1,24 @@
+
+
+var config={apiKey: "AIzaSyDAP80fa4j0abnhDWcLj07Jo9ayhmF3RZw",
+authDomain: "class-94-32630.firebaseapp.com",
+projectId: "class-94-32630",
+storageBucket: "class-94-32630.appspot.com",
+messagingSenderId: "698826182460",
+appId: "1:698826182460:web:fb5fcf2061cd81809ed839",
+measurementId: "G-LQX85JCTYK"};
+
+firebase.initializeApp(config);
+
+
+
+
+
+function addUser()
+{
+user_name=document.getElementById("user_name").value;
+firebase.database().ref("/").child(user_name).update({
+    purpose: "adding user"
+});
+
+}
